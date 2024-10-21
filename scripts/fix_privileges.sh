d1122e94d9079a80fcc6dfdae9878b69e8b9124b
Sagarika Ghom <130879668+sagarikaghom14@users.noreply.github.com>
2024-10-21 11:03:51 +0530

    Update fix_privileges.sh

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-# Make the start/stop scripts executable
+chmod +x /home/ec2-user/server/*  # This makes all files in the server directory executable
 chmod +x /home/ec2-user/server/server_start.sh
 chmod +x /home/ec2-user/server/server_stop.sh
