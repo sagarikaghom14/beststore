265989f01943c2c20a9b979bfee245ac5bddfdef
sagarikaghom14 <sagarika.ghom@mitaoe.ac.in>
2024-10-21 10:16:52 +0530

    routing changes

--- src/main/java/com/bootsmytool/beststore/BeststoreApplication.java
+++ src/main/java/com/bootsmytool/beststore/BeststoreApplication.java
@@ -2,13 +2,20 @@ package com.bootsmytool.beststore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BeststoreApplication {
+public class BeststoreApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeststoreApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BeststoreApplication.class);
+    }
 
 }
 
